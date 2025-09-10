37a1eba8f6931b7c6434ae9c095dfeac5d7cf527
damiankocher <damian@dmnkchr.com>
2025-09-09 21:12:15 -0500

    lock out submitting amendments

--- packages/convention-dashboard-client/src/views/MenuView/MenuView.tsx
+++ packages/convention-dashboard-client/src/views/MenuView/MenuView.tsx
@@ -1,17 +1,12 @@
 import {AppBar} from "../../components/AppBar/AppBar.tsx";
-import {goToFormsViewAction, goToFormViewAction} from "../../forms/formsSaga.ts";
 import {ButtonCard} from "../../components/ButtonCard/ButtonCard.tsx";
 import classes from "./MenuView.module.css"
 import {goToQuestionnaires} from "../../questionnaire/questionnairesSaga.ts";
 import {Footer} from "../../components/Footer/Footer.tsx";
 import {goToDocumentsView} from "../../document/documentSaga.ts";
-import {FormType, getHasForms} from "../../forms/formSlice.ts";
 import {Link} from "../../components/Link/Link.tsx";
-import {useSelector} from "react-redux";
 
 export const MenuView = () => {
-    const hasForms = useSelector(getHasForms);
-
     return (
         <>
             <AppBar/>
@@ -24,11 +19,6 @@ export const MenuView = () => {
                 </div>
 
                 <div className={classes.section}>
-                    <ButtonCard label="Submit an Amendment" action={goToFormViewAction(FormType.AMENDMENT)}/>
-                    {hasForms &&
-                        <ButtonCard label="View Your Form Submissions"
-                                    action={goToFormsViewAction()}/>
-                    }
                     <p>
                         <Link url="https://link.stldsa.org/convention_standing_rules">
                             Standing Rules of the 2025 St. Louis DSA Local Convention
